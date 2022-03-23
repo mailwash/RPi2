62db009b8e185ba29c67b2ee6c9db96b951ace4f
mailwash <miloszhermanowicz@gmail.com>
2022-03-23 14:50:37 +0100

    Update pliku SensHat.py

---
+++ SensHat.py
@@ -0,0 +1,11 @@
+from sense_emu import SenseHat
+
+def display_readings(hat):
+    
+    hat.set_pixels([pixel for row in screen for pixel in row])
+
+hat = SenseHat()
+
+while True:
+    display_readings(hat)
+    sleep(0.1)

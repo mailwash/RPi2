e7a4e8f6927a04a5006146ff5c4ebb334b87d1c3
mailwash <miloszhermanowicz@gmail.com>
2022-03-25 15:45:41 +0100

    Silniki.py poprawione w ten sosob aby byla tylko 1 komenda do sterowania silnikami dla lewej i prawej strony na raz

--- Silniki.py
+++ Silniki.py
@@ -2,15 +2,42 @@ import RPi.GPIO as IO
 from time import sleep
 
 class silnik():
-    def __init__(self,Wlacz,Wyjscie1,Wyjscie2):
-        self Wlacz = Wlacz
-        self Wyjscie1 = Wyjscie1
-        self Wyjscie2 = Wyjscie2
-        IO.setup(self.Wlacz,IO.OUT)
-        IO.setup(self.Wyjscie1,IO.OUT)
-        IO.setup(self.Wyjscie2,IO.OUT)
-        self.pwm = IO.pwm(self.Wyjscie,100)
-        self.pwm.start(0)
+    def __init__(self,WlaczA,Wyjscie1A,Wyjscie2A, WlaczB,Wyjscie1B,Wyjscie2B):
+        self WlaczA = WlaczA
+        self WlaczB = WlaczB
+        self Wyjscie1A = Wyjscie1A
+        self Wyjscie1B = Wyjscie1B
+        self Wyjscie2A = Wyjscie2A
+        self Wyjscie2B = Wyjscie2B 
+        IO.setup(self.WlaczA,IO.OUT)
+        IO.setup(self.Wyjscie1A,IO.OUT)
+        IO.setup(self.Wyjscie2A,IO.OUT)
+        IO.setup(self.WlaczB,IO.OUT)
+        IO.setup(self.Wyjscie1B,IO.OUT)
+        IO.setup(self.Wyjscie2B,IO.OUT)
+        self.pwmA = IO.PWM(self.WlaczA,100)
+        self.pwmA.start(0)
+        self.pwmB = IO.PWM(self.WlaczB,100)
+        self.pwmB.start(0)
+        
+    def Ruch(self, predkosc=0.5, skret=0, czas=0):
+        predkosc *= 100
+        skret *= 100
+        predkoscLewego = predkosc - skret
+        predkoscPrawego = predkosc + skret
+        
+        if predkoscLewego > 100: predkoscLewego=100
+        elif predkoscLewego <-100: predkoscLewego=-100
+        if predkoscPrawego > 100: predkoscPrawego=100
+        elif predkoscPrawego <-100: predkoscPrawego=-100
+        
+        self.pwmA.ChangeDutyCycle(abs(predkoscLewego))
+        self.pwmB.ChangeDutycycle(abs(predkoscPrawego))
+        
+        if predkoscLewego > 0: IO.output(self.Wyjscie1A,IO.HIGH); IO.output(self.Wyjscie2A,IO.LOW)
+        else: IO.output(self.Wyjscie1A,IO.LOW); IO.output(self.Wyjscie2A,IO.HIGH)
+        if predkoscPrawego >0: IO.output(self.Wyjscie1B,IO.HIGH); IO.output(self.Wyjscie2B,IO.LOW)
+        else: IO.output(self.Wyjscie1B,IO.LOW); IO.output(self.Wyjscie2B,IO.HIGH)
         
     def doPrzodu(self,Wypelnienie=50,Czas=0):
         IO.output(self.Wyjscie1,IO.HIGH)
@@ -23,7 +50,8 @@ class silnik():
         self.pwm.ChangeDutyCycle(Wypelnienie)
         sleep(Czas)
     def stop(self,Czas=0):
-        self.pwm.ChangeDutyCycle(0)
+        self.pwmA.ChangeDutyCycle(0)
+        self.pwmB.ChangeDutyCycle(0)
         sleep(Czas)
         
 LewyPrzod = silnik(2,3,4)

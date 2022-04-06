a565149c91a45f6dd7fc260067e0359d7ff1eaa0
mailwash <miloszhermanowicz@gmail.com>
2022-04-06 19:37:20 +0200

    keyboard class created

--- keyboard.py
+++ keyboard.py
@@ -1,20 +1,20 @@
 import pygame
-
+      
 class Sterowanie():
     def __init__(self):
         pygame.init()
         win = pygame.display.set_mode((200,200))
-        
-    def SprawdzKlawisz(self, klawisz):
-        byl = False
-        for event in pygame.event.get():pass
-        keyInput = pygame.key.get_pressed()
-        mojKlawisz = getattr(pygame,'K_{}'.format(klawisz))
-        if keyInput [mojKlawisz]:
-            byl = True
-        pygame.display.update()
-        
-        return byl
+    
+    def WezKlawisz(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                print(pygame.key.name(event.key))
+                if event.key == pygame.K_q:
+                    print('q pressed - quiting app')
+                    return 'q'
+                if event.key == pygame.K_LEFT:
+                    print('L arrow pressed ')
+                    return 'LEFT'
 
 try:
     klaw = Sterowanie()
@@ -22,13 +22,14 @@ try:
     RC = 0
     flagaWyjscia = True
     while flagaWyjscia:
-        if klaw.SprawdzKlawisz('LEFT'):
+        klawisz = klaw.WezKlawisz()
+        if klawisz == 'LEFT':
             LC += 1
             print('Wcisnieta strzałka w lewo ' + str(LC) + ' razy')
-        if klaw.SprawdzKlawisz('RIGHT'):
-            RC += 1
-            print('Wcisnieta strzałka w prawo ' + str(RC) + ' razy')
-        if klaw.SprawdzKlawisz('q'):
+#         if klaw.SprawdzKlawisz('RIGHT'):
+#             RC += 1
+#             print('Wcisnieta strzałka w prawo ' + str(RC) + ' razy')
+        if klawisz == 'q':
             print('Zamknij apke')
             flagaWyjscia=False
             
